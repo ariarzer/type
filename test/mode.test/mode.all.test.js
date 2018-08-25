d6dd51cc3d9d754f8956749b40b25ab34abed0e9
Sofi Valitova <ariarzer@gmail.com>
2018-08-25 15:47:47 +0300

    Added tests for the mode 'all'

---
+++ test/mode.test/mode.all.test.js
@@ -0,0 +1,38 @@
+const Type = require('../../index.js');
+
+if (Type.create) {
+  const type = Type.create('all');
+}
+
+function MyObject() {
+  this.prop = -1;
+}
+
+const cases = [
+  { name: 'undefined', value: undefined },
+  { name: 'null', value: null },
+  { name: 'string', value: '' },
+  { name: 'number', value: 1 },
+  { name: 'number', value: NaN },
+  { name: 'boolean', value: true },
+  { name: 'object', value: {} },
+  { name: 'array', value: [] },
+  { name: 'symbol', value: Symbol('test') },
+  { name: 'map', value: new Map() },
+  { name: 'set', value: new Set() },
+  { name: 'weakmap', value: new WeakMap() },
+  { name: 'weakset', value: new WeakSet() },
+  { name: 'date', value: new Date() },
+  { name: 'myobject', value: new MyObject(2) },
+  { name: 'error', value: new Error() },
+  { name: 'typeerror', value: new TypeError() },
+  { name: 'regexp', value: /a/ },
+  { name: 'promise', value: new Promise(function (resolve, reject) {}) },
+  { name: 'htmldivelement', value: document.createElement('div') },
+];
+
+cases.forEach(({ name, value }) => {
+  test.skip(`for "${name}"`, () => {
+    expect(type(value)).toBe(name);
+  });
+});
