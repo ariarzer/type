6de4f728c850cf502ce15b902af4b29bcf04732f
Sofi Valitova <ariarzer@gmail.com>
2018-08-25 15:48:53 +0300

    Added tests for the wrong mode

---
+++ test/mode.test/mode.wrong.test.js
@@ -0,0 +1,40 @@
+const Type = require('../../index.js');
+
+if (Type.create) {
+  const type = Type.create('abcd');
+}
+
+const errorMessage = 'Wrong mode';
+
+function MyObject() {
+  this.prop = -1;
+}
+
+const cases = [
+  undefined,
+  null,
+  '',
+  1,
+  NaN,
+  true,
+  {},
+  [],
+  Symbol('test'),
+  new Map(),
+  new Set(),
+  new WeakMap(),
+  new WeakSet(),
+  new Date(),
+  new MyObject(2),
+  new Error(),
+  new TypeError(),
+  /a/,
+  new Promise((resolve, reject) => {}),
+  document.createElement('div'),
+];
+
+cases.forEach((item) => {
+  test.skip('for wrong mode', () => {
+    expect(() => type(item)).toThrowError(errorMessage);
+  });
+});
